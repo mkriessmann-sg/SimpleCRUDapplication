082a43679426b022851eb94c761da5d6f1e22a2d
MARCO KRIESSMANN <kriessmann-marco@s-giken.com>
2024-04-04 11:07:04 +0900

    Final Commit

--- SimpleCRUDapplication/Program.cs
+++ SimpleCRUDapplication/Program.cs
@@ -18,77 +18,41 @@ namespace SimpleCRUDapplication
     {
         List<Book> books = new List<Book>();
         bool run = true;
-        string? input = null;
+        int? input = null;
 
 
 
-
+        //MAIN FUNCTIONS
         public void Execute()
         {
-            
-           
-                Console.Clear();
-                Console.WriteLine("Select Operation");
-                Console.WriteLine("1 Display Current Books");
-                Console.WriteLine("2 Add Books");
-                Console.WriteLine("3 Delete Books");
-                Console.WriteLine("4 Edit book information");
-                Console.WriteLine("5 Exit");
-                Console.Write("Please select operation:");
-                input = Console.ReadLine();
-
-                switch (input)
-                {
-                    case "1":
-                        DisplayBooks(books, "Main");
-                        break;
-                    case "2":
-
-                        break;
-                    case "3":
-
-                        break;
-                    case "4":
-
-                        break;
-                    case "5":
-                        Environment.Exit(0);
-                        break;
-                    default:
-
-                        Console.WriteLine("invalid input");
-                        break;
-                }
 
 
-
-        }
-        void DisplayBooks(List<Book> books, string context)
-        {
             Console.Clear();
-            foreach (Book b in books)
+            Console.WriteLine("Select Operation");
+            Console.WriteLine("1 Display Current Books");
+            Console.WriteLine("2 Add Books");
+            Console.WriteLine("3 Delete Books");
+            Console.WriteLine("4 Edit book information");
+            Console.WriteLine("5 Exit");
+            Console.Write("Please select operation:");
+            string tempinput = Console.ReadLine();
+            input = CheckForIntInput(tempinput);
+            Console.Clear();
+            switch (input)
             {
-                Console.WriteLine(b.identifier + ". " + b.title + " by" + b.author);
-
-            }
-            Console.Write("Press any Key to return");
-            Console.ReadKey();
-
-            switch (context)
-            {
-                case "Main":
-                    
+                case 1:
+                    DisplayBooks(books, "Main");
                     break;
-                case "2":
-
+                case 2:
+                    AddBook(books);
                     break;
-                case "3":
-
+                case 3:
+                    DeleteBook(books);
                     break;
-                case "4":
-
+                case 4:
+                    EditBook(books);
                     break;
-                case "5":
+                case 5:
                     Environment.Exit(0);
                     break;
                 default:
@@ -96,48 +60,69 @@ namespace SimpleCRUDapplication
                     Console.WriteLine("invalid input");
                     break;
             }
-            //TODO: Implement return to to screen with context
+
+
+
+        }
+        void DisplayBooks(List<Book> books, string context)
+        {
+            
+            foreach (Book b in books)
+            {
+                Console.WriteLine(b.identifier + ". " + b.title + " by" + b.author);
+
+            }
+            Console.Write("Press any Key to return");
+            Console.ReadKey();
+            Console.Clear();
+
+            switch (context)
+            {
+                case "Main":
+                    Execute();  
+                    break;
+                case "Add":
+                    AddBook(books);
+                    break;
+                case "Delete":
+                    DeleteBook(books);
+                    break;
+                case "Edit":
+                    EditBook(books);
+                    break;
+                
+                default:
+
+                    Console.WriteLine("unexpected error");
+                    break;
+            }
+            
 
         }
         void AddBook(List<Book> books)
         {
-            Console.Clear();
             Console.Write("Please enter Title:");
             string? title = Console.ReadLine();
             Console.Write("Please enter Author:");
             string? author = Console.ReadLine();
             Console.Write("Please enter identifier:");
-            //TODO: Add try and catch for invalid inputs
-            int tempidentifier = Convert.ToInt32(Console.ReadLine());
+            string? _tempInput = Console.ReadLine();
+            int tempidentifier = CheckForIntInput(_tempInput);
             int identifier = TestForDuplicate(books, tempidentifier);
 
             Book b = new Book(title, author, identifier);
             books.Add(b);
             Console.Write("Press any Key to return");
             Console.ReadKey();
-
+            Console.Clear();
+            Execute();
         }
 
-        int TestForDuplicate(List<Book> books, int identifier)
-        {
-            foreach (Book b in books)
-            {
-                if (b.identifier == identifier)
-                {
-                    Console.Write("Duplicate detected, please enter different desiered number:");
-                    //TODO: Add try and catch for invalid inputs
-                    identifier = Convert.ToInt32(Console.ReadLine());
-                    identifier = TestForDuplicate(books, identifier);
-                }
-            }
-
-            return identifier;
-        }
         //TODO: implement function
         void DeleteBook(List<Book> books)
         {
             int? identifier = null;
-            Console.Clear();
+            
             Console.Write("Please enter the identifyer of the book you would like to delete.\nIf you do not know the identifier of the book you would like to delete, enter 's' to display the list of books.\nPlease enter here:");
             string? _tempInput;
             _tempInput = Console.ReadLine();
@@ -148,8 +133,8 @@ namespace SimpleCRUDapplication
             }
             else
             {
-                //TODO: Implement catch for wrong input
-                identifier = Convert.ToInt32(_tempInput);
+
+                identifier = CheckForIntInput(_tempInput);
             }
 
             foreach (Book b in books)
@@ -160,7 +145,11 @@ namespace SimpleCRUDapplication
                 }
                 else
                 {
-                    //TODO:Implement wrong input
+                    Console.WriteLine("The selected idenfifier does not exist.");
+                    DeleteBook(books);
+                    //Return immedeately to improve performance
+                    return;
+
                 }
             }
             foreach (Book b in books)
@@ -171,35 +160,143 @@ namespace SimpleCRUDapplication
                     books.Remove(b);
                 }
             }
-
+            Console.Clear();
+            Execute (); 
 
         }
 
         //TODO: implement function
         void EditBook(List<Book> books)
         {
+            int? identifier = null;
 
+            Console.Write("Please enter the identifyer of the book you would like to delete.\nIf you do not know the identifier of the book you would like to delete, enter 's' to display the list of books.\nPlease enter here:");
+            string? _tempInput;
+            _tempInput = Console.ReadLine();
+            if (_tempInput == "s")
+            {
+                DisplayBooks(books, "Delete");
+
+            }
+            else
+            {
+
+                identifier = CheckForIntInput(_tempInput);
+            }
+
+            foreach (Book b in books)
+            {
+                if (b.identifier == identifier)
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("The selected idenfifier does not exist.");
+                    DeleteBook(books);
+                    //Return immedeately to improve performance
+                    return;
+
+                }
+            }
+            foreach (Book b in books)
+            {
+                if (b.identifier == identifier)
+                {
+                    EditBookInformation(b);
+                    
+                }
+            }
+            Console.Clear();
+            Execute();  
         }
 
 
 
 
-    }
 
-    class Book
-    {
-        public string? title;
-        public string? author;
-        public int identifier;
 
-        public Book(string? title, string? author, int identifier)
+
+
+
+        //SUPPLEMENTARY FUNCTIONS
+        int TestForDuplicate(List<Book> books, int identifier)
         {
-            this.title = title;
-            this.author = author;
-            this.identifier = identifier;
+            foreach (Book b in books)
+            {
+                if (b.identifier == identifier)
+                {
+                    Console.Write("Duplicate detected, please enter different desiered number:");
+                    string? _tempInput = Console.ReadLine();
+                    identifier = CheckForIntInput(_tempInput);
+                    identifier = TestForDuplicate(books, identifier);
+                }
+            }
+
+            return identifier;
+        }
+
+        int CheckForIntInput(string _input)
+        {
+
+
+            if (int.TryParse(_input, out int result))
+            {
+                return result; // Successfully parsed, return the integer
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+                string? _newInput = Console.ReadLine();
+                return CheckForIntInput(_newInput);
+            }
+        }
+
+        void EditBookInformation(Book book)
+        {
+            Console.WriteLine("Please Select the operation you would like to perform:\n1... Edit Title\n2...Edit Author\n3...Edit Both\nPlease enter here:");
+            string? _tempInput = Console.ReadLine();
+            int _selection = CheckForIntInput(_tempInput);
+
+            switch (input)
+            {
+                case 1:
+                    Console.WriteLine("Please enter new Title");
+                    book.title = Console.ReadLine();
+                    break;
+                case 2:
+                    Console.WriteLine("Please enter new Author");
+                    book.author = Console.ReadLine();
+                    break;
+                case 3:
+                    Console.WriteLine("Please enter new Title");
+                    book.title = Console.ReadLine();
+                    Console.WriteLine("Please enter new Author");
+                    book.author = Console.ReadLine();
+                    break;
+
+                default:
+                    Console.WriteLine("invalid input");
+                    EditBookInformation(book);
+                    break;
+            }
+
+        }
+
+        class Book
+        {
+            public string? title;
+            public string? author;
+            public int identifier;
+
+            public Book(string? title, string? author, int identifier)
+            {
+                this.title = title;
+                this.author = author;
+                this.identifier = identifier;
+            }
         }
     }
-
 }
 
 
